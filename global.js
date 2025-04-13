d10929f2c0e3e9a5b791c2c019978673c3598eaf
srangan29 <srangan@ucsd.edu>
2025-04-13 12:08:51 -0700

    functionality of color-scheme selector (1)

--- global.js
+++ global.js
@@ -69,3 +69,13 @@ document.body.insertAdjacentHTML(
               </select>
       </label>`,
   );
+  select = document.querySelector(selector)
+  select.addEventListener('input', function (event) {
+    console.log('color scheme changed to', event.target.value);
+  });
+  document.documentElement.style.setProperty('color-scheme', event.target.value);
+  
+  localStorage.colorScheme = event.target.value;
+  if ('colorScheme' in localStorage) {
+    select.value = localStorage.colorScheme
+  } 
