452d4aebfed5777c071b5e871ec3cb27fefb200f
srangan29 <srangan@ucsd.edu>
2025-04-20 23:31:04 -0700

    Create projects.js

---
+++ projects/projects.js
@@ -0,0 +1,16 @@
+import { fetchJSON, renderProjects } from '../global.js';
+const projects = await fetchJSON('../lib/projects.json');
+const projectsContainer = document.querySelector('.projects');
+renderProjects(projects, projectsContainer, 'h2');
+
+export function renderProjects(project, containerElement, headingLevel= 'h2') {
+    // Your code will go here
+    containerElement.innerHTML = '';
+        const article = document.createElement('article'); 
+        article.innerHTML = `
+        <h3>${project.title}</h3>
+        <img src="${project.image}" alt="${project.title}">
+        <p>${project.description}</p>
+`;
+        containerElement.appendChild(article);
+  }
