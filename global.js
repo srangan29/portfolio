c78c5f438cba89075f0ff8f0e8dc44112ccea3b0
srangan29 <srangan@ucsd.edu>
2025-04-25 16:31:08 -0700

    h3 to variable headingLevel

--- global.js
+++ global.js
@@ -105,7 +105,7 @@ document.body.insertAdjacentHTML(
     for (const project of projects) {
         const article = document.createElement('article'); 
         article.innerHTML = `
-        <h3>${project.title}</h3>
+        <${headingLevel}>${project.title}</${headingLevel}>
         <img src="${project.image}" alt="${project.title}">
         <p>${project.description}</p>
 `;
