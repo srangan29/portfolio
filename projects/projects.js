de758f79c0d4cb88735d74280636aa7068c13c72
srangan29 <srangan@ucsd.edu>
2025-05-02 12:45:34 -0700

    added legend

--- projects/projects.js
+++ projects/projects.js
@@ -34,3 +34,10 @@ arcs.forEach((arc, idx) => {
     .attr('fill', colors(idx)) // Fill in the attribute for fill color via indexing the colors variable
 })
 
+let legend = d3.select('.legend');
+data.forEach((d, idx) => {
+  legend
+    .append('li')
+    .attr('style', `--color:${colors(idx)}`) // set the style attribute while passing in parameters
+    .html(`<span class="swatch"></span> ${d.label} <em>(${d.value})</em>`); // set the inner html of <li>
+});
