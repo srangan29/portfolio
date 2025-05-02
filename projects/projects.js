0a66b8cbd4a014be464e7bd0072825606b4dcb81
srangan29 <srangan@ucsd.edu>
2025-05-01 19:45:15 -0700

    adding pie plot

--- projects/projects.js
+++ projects/projects.js
@@ -1,4 +1,6 @@
 import { fetchJSON, renderProjects } from '../global.js';
+import * as d3 from 'https://cdn.jsdelivr.net/npm/d3@7.9.0/+esm';
+
 const projects = await fetchJSON('../lib/projects.json');
 const projectsContainer = document.querySelector('.projects');
 renderProjects(projects, projectsContainer, 'h2');
@@ -9,3 +11,4 @@ renderProjects(projects, projectsContainer, 'h2');
   const title = document.querySelector('.projects-title');
   const projectCounter = projects.length
   title.textContent = `${projectCounter} Projects`;
+
