91a4af4ea01df212447c3d0086c17e693d40f2be
srangan29 <srangan@ucsd.edu>
2025-05-01 20:08:30 -0700

    specifying data w/ values

--- projects/projects.js
+++ projects/projects.js
@@ -12,14 +12,17 @@ renderProjects(projects, projectsContainer, 'h2');
   const projectCounter = projects.length
   title.textContent = `${projectCounter} Projects`;
 
-let arcGenerator = d3.arc().innerRadius(0).outerRadius(50);
-let arc = arcGenerator({
-  startAngle: 0,
-  endAngle: 2 * Math.PI,
-});
 
-let data = [1, 2, 3, 4, 5, 5];
-let sliceGenerator = d3.pie();
+
+let data = [
+  { value: 1, label: 'apples' },
+  { value: 2, label: 'oranges' },
+  { value: 3, label: 'mangos' },
+  { value: 4, label: 'pears' },
+  { value: 5, label: 'limes' },
+  { value: 5, label: 'cherries' },
+];
+let sliceGenerator = d3.pie().value((d) => d.value);
 let arcData = sliceGenerator(data);
 let arcs = arcData.map((d) => arcGenerator(d));
 let colors = d3.scaleOrdinal(d3.schemeTableau10);
