209dda90a837ba50f7e6ad34c39077f2bfd5b230
srangan29 <srangan@ucsd.edu>
2025-05-09 14:15:03 -0700

    Update main.js

--- meta/main.js
+++ meta/main.js
@@ -1,8 +1,39 @@
 import * as d3 from 'https://cdn.jsdelivr.net/npm/d3@7.9.0/+esm';
 async function loadData() {
-  const data = await d3.csv('loc.csv');
-  console.log(data);
+  const data = await d3.csv('loc.csv', (row) => ({
+    ...row,
+    line: Number(row.line), // or just +row.line
+    depth: Number(row.depth),
+    length: Number(row.length),
+    date: new Date(row.date + 'T00:00' + row.timezone),
+    datetime: new Date(row.datetime),
+  }));
+
   return data;
 }
 
+function processCommits(data) {
+  return d3
+    .groups(data, (d) => d.commit)
+    .map(([commit, lines]) => {
+      // Each 'lines' array contains all lines modified in this commit
+      // All lines in a commit have the same author, date, etc.
+      // So we can get this information from the first line
+      let first = lines[0];
+        
+        
+      // What information should we return about this commit?
+      return {
+        id: commit,
+        // ... what else?
+        author,
+        date,
+        time,
+        timezone,
+        datatime
+      };
+    });
+}
+
 let data = await loadData();
+let commits = d3.groups(data, (d) => d.commit);
