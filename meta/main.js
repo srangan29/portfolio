250b321e5e0dd829dd4350ff22a18497bdcd8c3c
srangan29 <srangan@ucsd.edu>
2025-05-09 14:31:21 -0700

    in progress

--- meta/main.js
+++ meta/main.js
@@ -21,19 +21,103 @@ function processCommits(data) {
       // So we can get this information from the first line
       let first = lines[0];
         
+      // We can use object destructuring to get these properties
+      let { author, date, time, timezone, datetime } = first;
         
       // What information should we return about this commit?
       return {
         id: commit,
         // ... what else?
+        url: 'https://github.com/vis-society/lab-7/commit/' + commit,
         author,
         date,
         time,
         timezone,
-        datatime
+        datatime,
+         // Calculate hour as a decimal for time analysis
+        // e.g., 2:30 PM = 14.5
+        hourFrac: datetime.getHours() + datetime.getMinutes() / 60,
+        // How many lines were modified?
+        totalLines: lines.length,
       };
+       Object.defineProperty(ret, 'lines', {
+        value: lines,
+        // What other options do we need to set?
+        // Hint: look up configurable, writable, and enumerable
+      });
+        return ret;
     });
 }
 
 let data = await loadData();
 let commits = d3.groups(data, (d) => d.commit);
+console.log(commits);
+
+function renderCommitInfo(data, commits) {
+  // Create the dl element
+  const dl = d3.select('#stats').append('dl').attr('class', 'stats');
+
+  // Add total LOC
+  dl.append('dt').html('Total <abbr title="Lines of code">LOC</abbr>');
+  dl.append('dd').text(data.length);
+
+  // Add total commits
+  dl.append('dt').text('Total commits');
+  dl.append('dd').text(commits.length);
+
+  // Add more stats as needed...
+  const fileLengths = d3.rollups(
+  data,
+  (v) => d3.max(v, (v) => v.line),
+  (d) => d.file,
+);
+const averageFileLength = d3.mean(fileLengths, (d) => d[1]);
+const workByPeriod = d3.rollups(
+  data,
+  (v) => v.length,
+  (d) => new Date(d.datetime).toLocaleString('en', { dayPeriod: 'short' }),
+);
+onst maxPeriod = d3.greatest(workByPeriod, (d) => d[1])?.[0];
+}
+
+let data = await loadData();
+let commits = processCommits(data);
+
+renderCommitInfo(data, commits);
+
+function renderScatterPlot(data, commits) {
+ // Put all the JS code of Steps inside this function
+const width = 1000;
+const height = 600;
+
+const svg = d3
+  .select('#chart')
+  .append('svg')
+  .attr('viewBox', `0 0 ${width} ${height}`)
+  .style('overflow', 'visible');
+
+const xScale = d3
+  .scaleTime()
+  .domain(d3.extent(commits, (d) => d.datetime))
+  .range([0, width])
+  .nice();
+
+const yScale = d3.scaleLinear().domain([0, 24]).range([height, 0]);
+
+const dots = svg.append('g').attr('class', 'dots');
+
+dots
+  .selectAll('circle')
+  .data(commits)
+  .join('circle')
+  .attr('cx', (d) => xScale(d.datetime))
+  .attr('cy', (d) => yScale(d.hourFrac))
+  .attr('r', 5)
+  .attr('fill', 'steelblue');
+}
+
+
+let data = await loadData();
+let commits = processCommits(data);
+
+renderScatterPlot(data, commits);
